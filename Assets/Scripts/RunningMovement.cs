3b9ef5c336548dae783b3898d5448e9f3602fca9
jeromej2 <jeromej2@hawkmail.newpaltz.edu>
2016-11-22 18:01:33 -0500

    Running Movement of Enemy

---
+++ Assets/Scripts/RunningMovement.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RunningMovement : MonoBehaviour
+{
+    new Rigidbody2D rigidbody2D;
+    public float speed = 2f;
+    public float distance = 10;
+    private float startingX;
+
+
+    // Use this for initialization
+    void Start()
+    {
+        rigidbody2D = GetComponent<Rigidbody2D>();
+        startingX = transform.position.x;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (transform.position.x < startingX - distance)
+        {
+            speed = -speed;
+        }
+
+        if (transform.position.x > startingX)
+        {
+            speed = -speed;
+        }
+        var v = rigidbody2D.velocity;
+        v.x = -speed;
+        rigidbody2D.velocity = v;
+
+
+    }
+}
